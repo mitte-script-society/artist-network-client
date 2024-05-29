a971534ddff01ddb86c19fcb4dfb5b85668d7ed2
Suaste Cherizola <jsuaste86@gmail.com>
2024-05-29 15:11:36 +0200

    auth logic integrated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProviderWrapper } from './context/auth.context.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProviderWrapper>
+          <App />
+      </AuthProviderWrapper>
+    </BrowserRouter> 
   </React.StrictMode>,
 )
