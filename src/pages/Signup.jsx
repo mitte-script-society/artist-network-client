cf6a618c8a2f2917b37b2fb477544197a09c95cc
Greg <gajdukow@protonmail.com>
2024-05-31 05:44:52 +0200

    fix conditional rendering and image uploads for artists

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,9 +6,6 @@ import uploadImage from "../services/file-upload.service";
 const API_URL = "http://localhost:5005";
 
 function Signup(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
   const [isArtistChecked, setIsArtistChecked] = useState(false)
   const [isGroupChecked, setIsGroupChecked] = useState(false)
   const [showGroupFields, setShowGroupFields] = useState(true) // this is set to false on first render by UseEffect
@@ -30,6 +27,21 @@ function Signup(props) {
 
         const newObject = {... newUser};
         newObject.picture = response.fileUrl;
+        setNewUser(newObject)
+        return 
+    })
+    .catch(err => console.log("Error while uploading the file: ", err));    
+  }
+
+  const handleArtistImage = (e) => {
+    const uploadData = new FormData();
+    uploadData.append("imageUrl", e.target.files[0]);
+    
+    uploadImage(uploadData)
+      .then(response => {
+
+        const newObject = {... newUser};
+        newObject.artistPictures = response.fileUrl;
         setNewUser(newObject)
         return 
     })
@@ -111,8 +123,6 @@ useEffect(() => {
 
     //   { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-    //   <p>Already have account?</p>
-    //   <Link to={"/login"}> Login</Link>
     // </div>
 
     <>
@@ -123,7 +133,7 @@ useEffect(() => {
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
-                <label for="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                <label for="username" className="block text-sm font-medium leading-6 text-gray-900">Username*</label>
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                     {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
@@ -135,14 +145,14 @@ useEffect(() => {
 
 
               <div className="sm:col-span-4">
-                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address*</label>
                 <div className="mt-2">
                   <input id="email" name="email" type="email" onChange={handleChange} autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
                 </div>
               </div>
 
               <div className="sm:col-span-4">
-                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Password*</label>
                 <div className="mt-2">
                   <input id="password" name="password" type="password" onChange={handleChange} autocomplete="password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
                 </div>
@@ -160,7 +170,7 @@ useEffect(() => {
               </div>
 
               <div className="sm:col-span-2 sm:col-start-1">
-                <label for="city" className="block text-sm font-medium leading-6 text-gray-900">City</label>
+                <label for="city" className="block text-sm font-medium leading-6 text-gray-900">City*</label>
                 <div className="mt-2">
                   <input type="text" name="city" id="city" onChange={handleChange} valueautocomplete="address-level2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
                 </div>
@@ -207,7 +217,7 @@ useEffect(() => {
 
             <>
                 <div className="sm:col-span-4">
-                  <label for="groupName" className="block text-sm font-medium leading-6 text-gray-900">Group Name</label>
+                  <label for="groupName" className="block text-sm font-medium leading-6 text-gray-900">Group Name*</label>
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                       {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
@@ -217,7 +227,7 @@ useEffect(() => {
                 </div>
 
                 <div className="sm:col-span-4">
-                  <label for="groupname" className="block text-sm font-medium leading-6 text-gray-900">Members</label>
+                  <label for="groupname" className="block text-sm font-medium leading-6 text-gray-900">Members*</label>
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                       {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
@@ -232,7 +242,7 @@ useEffect(() => {
 
 
                 <div className="col-span-full">
-                  <label for="artistDescription" className="block text-sm font-medium leading-6 text-gray-900">Description / Bio</label>
+                  <label for="artistDescription" className="block text-sm font-medium leading-6 text-gray-900">Description / Bio*</label>
                   <div className="mt-2">
                     <textarea id="artistDescription" name="artistDescription" onChange={handleChange} rows="3" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required/>
                   </div>
@@ -240,7 +250,7 @@ useEffect(() => {
                 </div>
 
                 <div className="sm:col-span-2">
-                  <label for="groupname" className="block text-sm font-medium leading-6 text-gray-900">Hourly Fee in €</label>
+                  <label for="groupname" className="block text-sm font-medium leading-6 text-gray-900">Hourly Fee in €*</label>
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                       {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
@@ -249,7 +259,7 @@ useEffect(() => {
                   </div>
                 </div>
 
-                <div className="col-span-full">
+                {/* <div className="col-span-full">
                   <label for="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
                   <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                     <div className="text-center">
@@ -266,7 +276,18 @@ useEffect(() => {
                       <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
                     </div>
                   </div>
+                </div> */}
+
+              <div className="col-span-full">
+                <label for="photo" className="block text-sm font-medium leading-6 text-gray-900">Photo</label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <svg className="h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                  </svg>
+                  {/* <button type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button> */}
+                  <input type="file" onChange={handleArtistImage} />
                 </div>
+              </div>
 
                 <div className="sm:col-span-4">
                   <label for="artistVideos" className="block text-sm font-medium leading-6 text-gray-900">YouTube Video Link</label>
@@ -322,6 +343,8 @@ useEffect(() => {
         <div className="mt-6 flex items-center justify-start gap-x-6">
           {/* <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button> */}
           <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
+          <p>Already have account?</p>
+          <Link to={"/login"}> Login</Link>
         </div>
       </form>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
