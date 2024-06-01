ce2fdb725fd2345ef223d2ed76ec39ccea3d36f0
Greg <gajdukow@protonmail.com>
2024-06-01 17:21:54 +0200

    add map view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,72 @@
 import DisplayConcerts from "../components/DiplayConcerts"
 import Login from "./Login"
 import Signup from "./Signup"
+import "leaflet/dist/leaflet.css"
+import { Link } from "react-router-dom"
+
+import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import {Icon, divIcon} from "leaflet"
+import MarkerClusterGroup from "react-leaflet-cluster"
+
+const markers = [
+  {
+    _id: 123456,
+    location: [52.52362,13.40720],
+    title: "Magic Jazz Evening",
+    date: "17 June 2024, 20h",
+    prices: 20
+  },
+  {
+    _id: 123456,
+    location: [52.53929,13.41371],
+    title: "Indie Rock Jam Session",
+    date: "20 June 2024, 19h",
+    prices: 15
+  },
+  {
+    _id: 123456,
+    location: [52.4615,13.3251],
+    title: "Soul Sisters Live",
+    date: "15 July 2024, 18h",
+    prices: 10
+  }
+];
+
+const customIcon = new Icon({
+  iconUrl: "/music.png",
+  iconSize: [38, 38]
+})
 
 export default function Home() {
 
-  return (
 
+  return (
     <div className="concerts-main-space">
 
       <div className="display-concerts-wrapper">
         <DisplayConcerts/>
       </div>
       <div className="map-space">
-        <h1 style={{textAlign:"center"}}>The map magic happens here</h1>
+        <MapContainer center={[52.52362,13.40720]} zoom={13}>
+          <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.de/{z}/{x}/{y}.png"
+          />
+        <MarkerClusterGroup chunkedLoading>
+          {markers.map(marker => (
+            <Marker position={marker.location} icon={customIcon}>
+              <Popup>
+              <h1 className="text-md font-bold underline leading-none">{marker.title}</h1>
+              <p className="text md leading-none">{marker.date}</p>
+              <p className="text md leading-none">Price: {marker.prices} €</p>
+              <Link to={`/concerts/${marker._id}`} className="text md leading-none">See Concert Details</Link>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+
+        </MapContainer>
+  
       </div>
   
     </div>
