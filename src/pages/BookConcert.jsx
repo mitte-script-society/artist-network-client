49847785ed605874bc84e993112db6cfedee6d40
Greg <gajdukow@protonmail.com>
2024-06-03 19:28:50 +0200

    adjust classname

--- src/pages/BookConcert.jsx
+++ src/pages/BookConcert.jsx
@@ -240,7 +240,7 @@ function BookConcert(props) {
               <div className="sm:col-span-2 sm:col-start-1">
                 <label for="date" className="block text-sm font-medium leading-6 text-gray-900">Date and Time</label>
                 <div className="mt-2">
-                  <input type="datetime-local" name="date" id="date" className="mt-1 block w-full" onChange={handleChange} valueautocomplete="address-level2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
+                  <input type="datetime-local" name="date" id="date" onChange={handleChange} valueautocomplete="address-level2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required />
                 </div>
               </div>
 
