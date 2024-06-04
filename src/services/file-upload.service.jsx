0e6eb6d2ef2036808b77f692d622d98f07a6726c
Greg <gajdukow@protonmail.com>
2024-06-04 13:22:16 +0200

    change API path

--- src/services/file-upload.service.jsx
+++ src/services/file-upload.service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({baseURL: "http://localhost:5005/"
+const api = axios.create({baseURL: import.meta.env.VITE_API_URL
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
