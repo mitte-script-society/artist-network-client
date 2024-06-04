60b168494199a361ce2fbf9da3e230da494967d4
Greg <gajdukow@protonmail.com>
2024-06-04 19:55:32 +0200

    adjust navbar, add toast to signup form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
 
   return (
      <nav style={{position:"sticky", top:"0", zIndex:"1"  }} className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* <!-- Mobile menu button--> */}
