2a6a570b75c40e7e453cdf87a42b0667066ca0d1
Suaste Cherizola <jsuaste86@gmail.com>
2024-07-17 13:55:04 +0200

    chat button uncommented, adding artist.name again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -211,10 +211,10 @@ function Navbar({showAlert, changeInNotifications}) {
               : "text-gray-300 hover:bg-pink-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           }>My Dashboard</NavLink>
 
-          {/* <NavLink to="/chat" className={({ isActive }) =>
+          <NavLink to="/chat" className={({ isActive }) =>
             isActive ? "bg-pink-800 text-white block rounded-md px-3 py-2 text-base font-medium"
               : "text-gray-300 hover:bg-pink-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          }>Chat</NavLink> */}
+          }>Chat</NavLink>
         </>
           }
         </div>
